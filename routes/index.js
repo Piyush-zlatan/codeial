cc688d158af70ed7564c7e4ce7e109b4655c2d1c
Piyush-zlatan <12piyush10@gmail.com>
2020-02-26 14:57:39 +0530

    set view engine and created router for profile

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ const homeController  = require('../controllers/home_controller');
 console.log('runnnint');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
+
+//for any further router, access from here
+//router.use('/routerName',require('./routerfile'))
 
 
 module.exports = router;
