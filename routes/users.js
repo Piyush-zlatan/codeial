973ef69a9b19e703b6f319198a7b064da9d4fae6
Piyush-zlatan <12piyush10@gmail.com>
2020-03-05 12:19:50 +0530

    add sign up and sign in forms and initial check

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,10 @@ const usersControllers = require('../controllers/users_controller');
 
 router.get('/profile',usersControllers.profile);
 
+router.get('/sign-up',usersControllers.signUp);
+router.get('/sign-in',usersControllers.signIn);
+
+router.post('/create',usersControllers.create);
+
 
 module.exports = router;
