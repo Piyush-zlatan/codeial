0a0d7ef3d2ef8af9d4ab014fa37fbc0393f20d73
Piyush-zlatan <12piyush10@gmail.com>
2020-03-09 19:46:07 +0530

    check authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+//here passport.checkAuthentication this will check if user is signed in or not..if not it won't reach to action in controller
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports = router;
