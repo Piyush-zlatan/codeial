476f55358fb7f79441973077615ff38b3e1d0208
Piyush-zlatan <12piyush10@gmail.com>
2020-03-09 18:25:05 +0530

    display post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
      //   return res.end('<h1>Express is up for codeial</h1>');
-        console.log(req.cookies);
+       // console.log(req.cookies);
+
+//        Post.find({}, function(err,posts){
+//         return res.render('home',{
+//                 title: 'Home',
+//                 posts: posts
+//            });
+
+//        });
+
+       //populate the user of each post
+       Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home',{
-                title: 'Home'
-        })
+                title: 'Home',
+                posts: posts
+           });
+   });
+        
 }
 
 //module.exports.actionName  = function(req,res)
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,17 @@
-module.exports.posts  = function(req,res){
+const Post = require('../models/post');
 
-    return res.end('<h1>Posts Page</h1>');
+module.exports.create  = function(req,res){
+    Post.create({
+        content:req.body.content,
+        user: req.user._id
+        },function(err,post){
+            if(err){console.log('error in creating a post'); return;}
+
+            return res.redirect('back');
+        }
+        
+    )
+
+    //return res.end('<h1>Posts Page</h1>');
 }
 
