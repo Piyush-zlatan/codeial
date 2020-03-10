812f51bbd4b2b21e32523fb3d67c633c19e9fbbf
Piyush-zlatan <12piyush10@gmail.com>
2020-03-10 13:11:30 +0530

    add comment to database

---
+++ routes/comments.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const commentsController = require('../controllers/comments_controller');
+
+//here passport.checkAuthentication this will check if user is signed in or not..if not it won't reach to action in controller
+router.post('/create',passport.checkAuthentication,commentsController.create);
+
+module.exports = router;
