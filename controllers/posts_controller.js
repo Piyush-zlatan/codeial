c2ee349327624b125ddb22b1c4d4d7c7fc87a971
Piyush-zlatan <12piyush10@gmail.com>
2020-03-15 11:20:52 +0530

    Saving uploaded files from params

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -57,7 +57,7 @@ module.exports.destroy = async function (req, res) {
 
    }catch(err){
     req.flash('error',err);
-    return;
+    return res.redirect('back');
    }
 
 }
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const multer = require('multer');
 const path = require('path');
-const AVATAR_PATH = path.join('/uploads/users/avatars');        //path where the image will be stored
+const AVATAR_PATH = path.join('/uploads/users/avatar');        //path where the image will be stored
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -32,9 +32,14 @@ let storage = multer.diskStorage({
       cb(null, path.join(__dirname,'..',AVATAR_PATH));
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now());
     }
   })
+
+  //static                              //Here we are assigning storage to multer and with single making sure that there should be one file only
+  userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar'); 
+  //Making avatar path publicaly accessible
+  userSchema.statics.avatarPath = AVATAR_PATH;
 
 const user = mongoose.model('user',userSchema);
 module.exports = user;
