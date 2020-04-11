9281f18e8e0bf0cc0e16e7be042e2388295b5148
Piyush-zlatan <12piyush10@gmail.com>
2020-04-11 11:13:37 +0530

    Production environment

--- config/environment.js
+++ config/environment.js
@@ -20,10 +20,11 @@ const development= {
     jwt_secret : 'codeial'
 }
 
+// make a file for environment variable and put the keys into that then access them like process.variablename
 const production={
     name:'production',
     assest_path:'./assets',
-    session_cookie_key:'blahSomething',
+    session_cookie_key:'5C1B2BB515DC5',
     db:'codeial_production',
     smtp:{
         service: 'gmail',
@@ -37,8 +38,8 @@ const production={
     },
     google_client_id: "58330172222-emd1rds9k7r58bsinjqn51spghf3l2hm.apps.googleusercontent.com",
     google_client_secret: "wzx6q4u9ZcSfe-4J89dOFIVL",
-    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret : 'codeial'
+    google_call_back_url: "http://codeial.com/users/auth/google/callback",
+    jwt_secret : '^If7|SyJhuzd;ZZmX'
 }
 
 module.exports = development;
